ce1cfa86615dbb6f21fb9742d10398f928a0b082
Naf <nnegussie7@gmail.com>
2022-07-19 18:23:05 -0400

    initial commit

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,15 @@
-// Assignment code here
 
-
-// Get references to the #generate element
+function userOptions() {
+    var numbers= confirm("do you want numbers")
+    console.log(numbers)
+}
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
 function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
 
+    var passwordText = document.querySelector("#password");
+    userOptions()
     passwordText.value = password;
 
 }
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
